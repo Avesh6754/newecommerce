aa24a0bff0878b5b7eab8bc61067e28b5af526f1
Avesh6754 <aweshprajapati150@gmail.com>
2024-08-01 12:55:10 +0530

    1-08-2024

--- lib/home/homescreen.dart
+++ lib/home/homescreen.dart
@@ -1,3 +1,5 @@
+// import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
+
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
@@ -12,135 +14,109 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Container(
-        height: h,
-        width: w,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(15, 30, 15, 10),
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: h,
+          width: w,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  height: 60,
-                  width: double.infinity,
-                  // margin: EdgeInsets.only(bottom: 24),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle, color: Colors.black),
-                      ),
-                      Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                                text: 'Welcome\n',
-                                style: TextStyle(fontSize: 15)),
-                            TextSpan(
-                                text: 'Welcome',
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Icon(
-                        Icons.notifications_none,
-                        size: 25,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(Icons.favorite_border, size: 25),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  height: 60,
-                  width: double.infinity,
-                  // margin: EdgeInsets.only(bottom: 15),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.grey),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Icon(
-                        Icons.search,
-                        size: 25,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('Search'),
-                      Spacer(),
-                      Icon(
-                        Icons.filter_alt_outlined,
-                        size: 25,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      )
-                    ],
-                  ),
-                ),
+                const SizedBox(height: 5),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Text(
-                      'Specal ',
+                    Container(
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                          border: Border.all(width: 1.4, color: Colors.black)),
                     ),
-                    Spacer(),
-                    Text(
-                      'Specal ',
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                              text: 'Hello\n',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 15,
+                                  color: Colors.grey)),
+                          TextSpan(
+                              text: 'Avesh ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 20,
+                                  color: Colors.black)),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 10,),
                 Row(
                   children: [
                     Container(
-                      color: Colors.green,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      margin: EdgeInsets.only(top: 15),
+                      height: 60,
+                      width: 358,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(12),
+                        
+                      ),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Container(
-                            height: 200,
-                            width: 180,
-                            decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(15)),
-                          ),
-                          Text('Name',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                          Row(
-                            children: [
-                              Text('rate'),
-                              SizedBox(width: 10,),
-                              Text('|'),
-                              Container(
-                                height: 20,
-                                width: 60,
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(5)),
-                              )
-                            ],
-                          )
+                          SizedBox(width: 10,),
+                          Icon(Icons.search,size: 35,color: Colors.grey,),
+                          SizedBox(width: 10,),
+                          Text('Search for shoes',style: TextStyle(fontSize: 20,color: Colors.grey,fontWeight: FontWeight.w600),)
                         ],
                       ),
-                    )
+                    ),
+                    SizedBox(width: 10,),
+                    Container(
+                      margin: EdgeInsets.only(top: 15),
+                      height: 60,
+                      width:60,
+
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.grey.shade300
+                      ),
+                      child: Icon(Icons.tune,color: Colors.purple,size: 35,),
+                      
+                    ),
                   ],
+                ),
+                SizedBox(height: 15,),
+                textmethodforbold(text: 'Special offer'),
+                SizedBox(height: 15,),
+                Container(
+                  height: 130,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 15,),
+                      Text.rich(TextSpan(children: [
+                        TextSpan(text: '25% Off',style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.w600))
+                      ]))
+                    ],
+                  ),
                 )
               ],
             ),
@@ -149,4 +125,6 @@ class _HomescreenState extends State<Homescreen> {
       ),
     );
   }
+
+  Text textmethodforbold({required String text}) => Text('$text',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 25),);
 }
